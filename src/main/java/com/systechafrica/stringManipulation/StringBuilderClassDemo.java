ac09eea912eb6308cf98aa0fdf16257d14cbf71b
Humphrey-nyaga <humphreynyaga254@gmail.com>
2023-08-29 00:25:18 -0400

    String builder

--- src/main/java/com/systechafrica/stringManipulation/StringBuilderClassDemo.java
+++ src/main/java/com/systechafrica/stringManipulation/StringBuilderClassDemo.java
@@ -1,4 +1,18 @@
 package com.systechafrica.stringManipulation;
 
+import java.util.logging.Logger;
+
 public class StringBuilderClassDemo {
+    private static final Logger LOGGER = Logger.getLogger(StringBuilderClassDemo.class.getName());
+
+    public static void main(String[] args) {
+
+
+    StringBuilder sb = new StringBuilder(100);
+    String str = "";
+    sb.append("Hello");
+
+    boolean equals = str.equals(sb.toString());
+    LOGGER.info("" + equals);
+    }
 }
