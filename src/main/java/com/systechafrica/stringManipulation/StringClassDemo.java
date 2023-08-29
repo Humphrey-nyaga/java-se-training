3d203d78537251f24431645ffe6e89a8615cebc6
Humphrey-nyaga <humphreynyaga254@gmail.com>
2023-08-29 00:32:40 -0400

    String comparison

--- src/main/java/com/systechafrica/stringManipulation/StringClassDemo.java
+++ src/main/java/com/systechafrica/stringManipulation/StringClassDemo.java
@@ -9,9 +9,24 @@ public class StringClassDemo {
 
     //? stringsa re immutable therefore cannot be modified once created
     //? unless they are reassigned
+    public static void main(String[] args) {
+
 
     String message = "Hello ";
     //! no effect
     // has effect by reassigning
+     LOGGER.info(message.concat("world"));
     String msg = message.concat("World");
+    LOGGER.info(msg);
+
+    //
+    String sb = new String("Hello, world!");
+    String sa = "Hello, world";
+    boolean equals = sa.equals(sb);
+    boolean equals2 = sa == sb;
+
+    LOGGER.info(""+equals);
+    LOGGER.info(""+equals2);
+
+    }
 }
