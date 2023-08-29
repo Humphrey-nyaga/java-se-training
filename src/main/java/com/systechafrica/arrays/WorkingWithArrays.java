04ac94cf13017c66dc15c2c4827772a2da7a7cc4
Humphrey-nyaga <humphreynyaga254@gmail.com>
2023-08-29 00:55:13 -0400

    one-dimensional arrays

---
+++ src/main/java/com/systechafrica/arrays/WorkingWithArrays.java
@@ -0,0 +1,21 @@
+package com.systechafrica.arrays;
+
+import com.systechafrica.stringManipulation.StringBuilderClassDemo;
+
+import java.util.logging.Logger;
+
+public class WorkingWithArrays {
+    private static final Logger LOGGER = Logger.getLogger(WorkingWithArrays.class.getName());
+    public void singleDimensionalArray(){
+        int[] numbers = {45, 10, 26,35, 40,59};
+        String[] nicknames = {"Mhusika", "Ntate", "Yokana"};
+        for (int number : numbers) {
+            LOGGER.info("" + number);
+        }
+    }
+    public static void main(String [] args){
+        WorkingWithArrays app = new WorkingWithArrays();
+        app.singleDimensionalArray();
+
+    }
+}
