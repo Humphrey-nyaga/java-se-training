fc5deb82c7de46ee9d982f76a01c10a18996ac77
Humphrey-nyaga <humphreynyaga254@gmail.com>
2023-08-30 00:42:17 -0400

    2D Arrays

--- src/main/java/com/systechafrica/arrays/WorkingWithArrays.java
+++ src/main/java/com/systechafrica/arrays/WorkingWithArrays.java
@@ -40,7 +40,7 @@ public class WorkingWithArrays {
         matrix2[2][0] = 20;
         matrix2[2][2] = 20;
 
-
+        //iterate through the 2d array
         for(int i = 0; i < matrix2.length; i++){
             for (int j = 0; j <matrix2[i].length ; j++) {
                 System.out.print(matrix2[i][j] + " ");
