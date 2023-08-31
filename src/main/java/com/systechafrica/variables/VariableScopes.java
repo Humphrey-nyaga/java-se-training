6c69bb9b0ed4d4ab1b9b8eeefba269275283b4ab
Humphrey-nyaga <humphreynyaga254@gmail.com>
2023-08-30 23:59:16 -0400

    variable scopes

---
+++ src/main/java/com/systechafrica/variables/VariableScopes.java
@@ -0,0 +1,35 @@
+package com.systechafrica.variables;
+
+import java.util.logging.Logger;
+
+public class VariableScopes {
+    private static final Logger LOGGER = Logger.getLogger(VariableScopes.class.getName());
+
+    //class variables => they are defined as variables and declared outside any class method
+    //class scope variables
+    private int matchesPlayed = 3;
+    public void simpleMethod(){
+        LOGGER.info("simple method output: "+matchesPlayed);
+    }
+    public void complexMethod(){
+        LOGGER.info("complex method output: " + matchesPlayed);
+    }
+    public int addTwoNumbers(int a, int b){ //method variable scope => variables
+        // declared as parameters in the outermost part of the method.
+        int result = a + b;
+
+        for (int i = 0; i < 5; i++) {
+            LOGGER.info("method output: " + i);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        VariableScopes app = new VariableScopes();
+        app.complexMethod();
+        app.simpleMethod();
+        app.addTwoNumbers(5,3);
+        LOGGER.info("main method output: " + app.matchesPlayed);
+
+    }
+}
