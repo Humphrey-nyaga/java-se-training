e5f1c5c0ce31bdab3ca747c26651b9d3a7ac01f5
Humphrey-nyaga <humphreynyaga254@gmail.com>
2023-08-31 08:41:58 -0400

    Add tests

---
+++ src/test/java/com/systechafrica/atmmachine/ATMMachineTest.java
@@ -0,0 +1,42 @@
+package com.systechafrica.atmmachine;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ATMMachineTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void isValidLogin() {
+    }
+
+    @Test
+    void checkBalance() {
+    }
+
+    @Test
+    void deposit() {
+    }
+
+    @Test
+    void withdraw() {
+    }
+
+    @Test
+    void transfer() {
+    }
+
+    @Test
+    void initiateTransaction() {
+    }
+}
