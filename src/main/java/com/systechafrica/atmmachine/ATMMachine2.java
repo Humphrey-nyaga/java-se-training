28e786e40a764295719e3bb2f847821cbec93664
Humphrey-nyaga <humphreynyaga254@gmail.com>
2023-09-04 00:32:31 -0400

    Create login functionality

--- src/main/java/com/systechafrica/atmmachine/ATMMachine2.java
+++ src/main/java/com/systechafrica/atmmachine/ATMMachine2.java
@@ -6,10 +6,27 @@ import java.util.logging.Logger;
 public class ATMMachine2 {
     private static final Logger LOGGER = Logger.getLogger(ATMMachine2.class.getName());
     Scanner scanner = new Scanner(System.in);
+    final String DEFAULT_USERNAME = "user254";
+    final String DEFAULT_PASSWORD = "Admin123";
     final double INITIAL_BALANCE = 1000.00;
     final double WITHDRAWAL_CHARGES = 0.02;
     double runningBalance = INITIAL_BALANCE;
-    public void login(){
+    public boolean login(){
+        //? try three times only, exit if not logged in
+        int loginTries = 1;
+        boolean loggedIn = false;
+        while(loginTries <= 3){
+            System.out.print("Enter your password: ");
+            String userPassword = scanner.nextLine();
+            if(userPassword.equals(DEFAULT_PASSWORD)){
+               // show menu
+                loggedIn = true;
+                break;
+            }
+            System.out.println("Wrong password.");
+            loginTries++;
+        }
+        return loggedIn;
 
     }
     public void displayMenu(){}
@@ -36,5 +53,12 @@ public class ATMMachine2 {
 
     public static void main(String[] args) {
         ATMMachine2 app = new ATMMachine2();
+        boolean loggedIn = app.login();
+        if(loggedIn){
+            System.out.println("Logged in successfully");
+            //show menu
+        }else{
+            System.out.println("Maximum attempts failed");
+        }
     }
 }
