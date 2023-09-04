9845cd697c8663a9cbd5aa69eca6d6db8298cad2
Humphrey-nyaga <humphreynyaga254@gmail.com>
2023-09-04 01:10:00 -0400

    ATM Simulation

--- src/main/java/com/systechafrica/atmmachine/ATMMachine2.java
+++ src/main/java/com/systechafrica/atmmachine/ATMMachine2.java
@@ -11,15 +11,16 @@ public class ATMMachine2 {
     final double INITIAL_BALANCE = 1000.00;
     final double WITHDRAWAL_CHARGES = 0.02;
     double runningBalance = INITIAL_BALANCE;
-    public boolean login(){
+
+    public boolean login() {
         //? try three times only, exit if not logged in
         int loginTries = 1;
         boolean loggedIn = false;
-        while(loginTries <= 3){
+        while (loginTries <= 3) {
             System.out.print("Enter your password: ");
             String userPassword = scanner.nextLine();
-            if(userPassword.equals(DEFAULT_PASSWORD)){
-               // show menu
+            if (userPassword.equals(DEFAULT_PASSWORD)) {
+                // show menu
                 loggedIn = true;
                 break;
             }
@@ -29,36 +30,105 @@ public class ATMMachine2 {
         return loggedIn;
 
     }
-    public void displayMenu(){}
-    public void checkBalance(){
-        System.out.println("Your balance is: "+ runningBalance);
+
+    public void displayMenu() {
+        System.out.println("**************************************");
+        System.out.println("*************ATM SIMULATOR************");
+        System.out.println("**************************************");
+        System.out.println("************ATM SERVICES**************");
+        System.out.println("______________________________________");
+        System.out.println(
+                "1. Check Balance\n" +
+                        "2. Deposit\n" +
+                        "3. Withdraw\n" +
+                        "4. Transfer Cash\n" +
+                        "5. Quit");
+        System.out.println("\n");
     }
-    public void performDeposit(){
+
+    public void checkBalance() {
+        System.out.println("Your balance is: " + runningBalance);
+    }
+
+    public void performDeposit() {
         //? get amount to deposit
         double amountToDeposit = scanner.nextDouble();
         //? add amount to running balance
         runningBalance += amountToDeposit;
     }
-    public void performWithdrawal(){
+
+    public void performWithdrawal() {
         //? get amount to withdraw
         //? calculate withdraw + charges
         //? check if running balance is sufficient
     }
-    public void performTransfer(){
+
+    public void performTransfer() {
         //? get transfer amount
         //? subtract amount from running balance
     }
-    public void printReceipt(){}
+
+    public void printReceipt() {
+    }
 
 
     public static void main(String[] args) {
         ATMMachine2 app = new ATMMachine2();
         boolean loggedIn = app.login();
-        if(loggedIn){
+        if (loggedIn) {
             System.out.println("Logged in successfully");
-            //show menu
-        }else{
-            System.out.println("Maximum attempts failed");
+            boolean keepShowingMenu = true;
+            while (keepShowingMenu) {
+                app.displayMenu();
+              try {
+                  System.out.print("Choose your option: ");
+                  int option = app.scanner.nextInt();
+                  switch (option) {
+                      case 1:
+                          app.checkBalance();
+                          break;
+                      case 2:
+                          app.performDeposit();
+                          break;
+                      case 3:
+                          app.performWithdrawal();
+                          break;
+                      case 4:
+                          app.performTransfer();
+                          break;
+                      case 5:
+                          keepShowingMenu = false;
+                          break;
+                      default:
+                          System.out.println("Invalid option...Try Again!");
+                          break;
+                  }
+              }catch (Exception e){
+                app.scanner.nextLine();
+                System.out.println("Only Integers are allowed!!");
+              }
+            }
         }
+
+
+//                if(option == 1){
+//                    app.checkBalance();
+//                } else if (option == 2) {
+//                    app.performDeposit();
+//                } else if (option == 3) {
+//                    app.performWithdrawal();
+//
+//                } else if (option == 4) {
+//                    app.performTransfer();
+//                } else if (option == 5) {
+//                    keepShowingMenu =  false;
+//                } else {
+//                    System.out.println("Invalid option...Try Again");
+//                }
+//            }
+//        }else{
+//            System.out.println("Maximum attempts failed");
+//        }
+
     }
 }
