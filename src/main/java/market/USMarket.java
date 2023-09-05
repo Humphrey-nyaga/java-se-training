71156d92926daa39a2f2375b3791069096314772
Humphrey-nyaga <45116952+Humphrey-nyaga@users.noreply.github.com>
2023-09-05 01:12:24 -0400

    package management

---
+++ src/main/java/market/USMarket.java
@@ -0,0 +1,13 @@
+package market;
+
+import sales.SalesMan;
+import sales.products.Product;
+
+public class USMarket {
+    SalesMan sm;
+    Product p;
+
+    //package declaration using fully qualified name
+//    sales.SalesMan sm;
+//    sales.products.Product pr;
+}
