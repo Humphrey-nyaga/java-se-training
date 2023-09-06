52e8bce896b624dc8ef56b0bd8286e3bb9e82947
Humphrey-nyaga <humphreynyaga254@gmail.com>
2023-09-06 00:20:39 -0400

    Access Modifiers

--- src/main/java/com/systechafrica/part2/accessmodifiers/AccessModifiersDemo.java
+++ src/main/java/com/systechafrica/part2/accessmodifiers/AccessModifiersDemo.java
@@ -1,4 +1,19 @@
 package com.systechafrica.part2.accessmodifiers;
 
+import java.util.logging.Logger;
+
 public class AccessModifiersDemo {
+    private static final Logger LOGGER = Logger.getLogger(AccessModifiersDemo.class.getName());
+
+    public static void main(String[] args) {
+        Person person2 = new Person("John Doe", "johndoe@example.com");
+        person2.printPersonalDetails();
+
+        Person person = new Person();
+        person.setName("Smith Doe");
+        person.setEmail("Smithdoe@example.com");
+
+        System.out.println("Name is: " + person.getName() + " and Email: " +         person.getEmail()
+        );
+    }
 }
