32d823f57bde72114796280bcd63dfa2aa42b4a4
Humphrey-nyaga <humphreynyaga254@gmail.com>
2023-09-06 00:21:23 -0400

    private and private access modifiers

--- src/main/java/com/systechafrica/part2/accessmodifiers/Person.java
+++ src/main/java/com/systechafrica/part2/accessmodifiers/Person.java
@@ -1,2 +1,47 @@
-package com.systechafrica.part2.accessmodifiers;public class Person {
+package com.systechafrica.part2.accessmodifiers;
+
+public class Person {
+    private String name;
+    private String email;
+    private boolean promoted;
+
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+    //setters and getters
+
+    public Person() {
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean isPromoted() {
+        return promoted;
+    }
+
+    public void setPromoted(boolean promoted) {
+        this.promoted = promoted;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void printPersonalDetails() {
+        System.out.println(getPersonDetails());
+    }
+
+    private String getPersonDetails() {
+        return email + " " + name;
+    }
 }
