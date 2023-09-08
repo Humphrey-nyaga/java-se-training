bd3569370b5aebc1689e734f97e27e6622fc44fb
Humphrey-nyaga <humphreynyaga254@gmail.com>
2023-09-08 17:05:17 -0400

    Handle orders and payments

--- src/main/java/com/systechafrica/restaurant/Restaurant.java
+++ src/main/java/com/systechafrica/restaurant/Restaurant.java
@@ -1,22 +1,77 @@
 package com.systechafrica.restaurant;
 
+import java.util.*;
 import java.util.logging.Logger;
 
 public class Restaurant {
     private static final Logger LOGGER = Logger.getLogger(Restaurant.class.getName());
+    private final Scanner scanner = new Scanner(System.in);
+    List<Integer> orderedMealsID = new ArrayList<>();
 
-    public void addMealsToMenu(){
-        Meal[] meals = new Meal[] {
-                new Meal(1, "CHAI", "15"),
-                new Meal(2, "ANDAZI", "10"),
-                new Meal(3, "TOSTI", "12"),
-                new Meal(4, "NDENGU AND ACCOMPLISHMENTS", "70"),
-                new Meal(5, "BEANS AND ACCOMPLISHMENTS", "70"),
-                new Meal(6, "PILAU VEG", "90")
-        };
-}
-    public void menu(){
-        System.out.println( "********** WELCOME ***********");
+    public void billing(List<Integer>mealsToBillList) {
+
+        List<Meal> mealsList = new ArrayList<>();
+        mealsList.add(new Meal(1, "CHAI", 15));
+        mealsList.add(new Meal(2, "ANDAZI", 10));
+        mealsList.add(new Meal(3, "TOSTI", 12));
+        mealsList.add(new Meal(4, "NDENGU AND ACCOMPLISHMENTS", 70));
+        mealsList.add(new Meal(5, "BEANS AND ACCOMPLISHMENTS", 70));
+        mealsList.add(new Meal(6, "PILAU VEG", 90));
+
+        List<String> orderedMeals = new ArrayList<>();
+        double totalPrice = 0;
+
+        for (Integer orderedID : mealsToBillList) {
+            for (Meal meal : mealsList) {
+                if (meal.getId() == orderedID) {
+                    String name = meal.getName();
+                    double price = meal.getPrice();
+                    orderedMeals.add(name + "\t" + price);
+                    totalPrice += price;
+                    break;
+                }
+            }
+        }
+
+        System.out.println("Pay Now For: ");
+        for (String meal : orderedMeals) {
+            System.out.println(meal);
+        }
+        System.out.println("Total: " + totalPrice);
+
+
+    }
+
+    public void order() {
+        menu();
+        System.out.print("Enter Your Meal/Drink Option: ");
+        int mealID = scanner.nextInt();
+        orderedMealsID.add(mealID);
+        scanner.nextLine();
+        userMenu();
+        }
+
+     public void userMenu(){
+         System.out.print("Do you want to enter another meal/drink option(Y/N): ");
+         Character orderAnotherMeal = scanner.next().charAt(0);
+         if(orderAnotherMeal.equals('Y')){
+             order();
+         } else{
+             System.out.print("Proceed to Payment(Y/N): ");
+             Character proceedToPayment = scanner.next().charAt(0);
+             if(proceedToPayment.equals('Y')){
+                 billing(orderedMealsID);
+             }
+         }
+
+     }
+
+    private void payments() {
+
+    }
+
+    public void menu() {
+        System.out.println("********** WELCOME ***********");
         System.out.println("****** SYSTECH RESTAURANT ****");
         System.out.println();
         System.out.printf("%-30s%n", "DRINKS");
@@ -34,11 +89,11 @@ public class Restaurant {
     public static void main(String[] args) {
         Authentication authentication = new Authentication();
         Restaurant restaurant = new Restaurant();
-        restaurant.addMealsToMenu();
-        if(authentication.login()){
-            restaurant.menu();
-        }else {
-            LOGGER.warning("Failed to Login");
-        }
+        restaurant.order();
+//        if (authentication.login()) {
+//            restaurant.menu();
+//        } else {
+//            LOGGER.warning("Failed to Login");
+//        }
     }
 }
