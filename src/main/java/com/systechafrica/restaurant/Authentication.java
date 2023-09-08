d34539a7313f30fb9ede9bbaa849ce0945f8ff7c
Humphrey-nyaga <humphreynyaga254@gmail.com>
2023-09-08 12:01:36 -0400

    restaurant authentication

--- src/main/java/com/systechafrica/restaurant/Authentication.java
+++ src/main/java/com/systechafrica/restaurant/Authentication.java
@@ -1,4 +1,26 @@
 package com.systechafrica.restaurant;
 
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class Authentication {
+    private static final Logger LOGGER = Logger.getLogger(Authentication.class.getName());
+    final String DB_PASSWORD = "Admin123";
+
+    public boolean login() {
+        int loginTriesCount = 0;
+        while (loginTriesCount < 3) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter password: ");
+            String password = scanner.nextLine();
+
+            if (password.equals(DB_PASSWORD)) {
+                return true;
+            } else
+                System.out.println("Incorrect username or password!!");
+            loginTriesCount++;
+        }
+        LOGGER.warning("Login Attempts Limit Exceeded!");
+        return false;
+    }
 }
--- src/main/java/com/systechafrica/restaurant/Restaurant.java
+++ src/main/java/com/systechafrica/restaurant/Restaurant.java
@@ -3,5 +3,6 @@ package com.systechafrica.restaurant;
 public class Restaurant {
     public static void main(String[] args) {
         Authentication authentication = new Authentication();
+        authentication.login();
     }
 }
