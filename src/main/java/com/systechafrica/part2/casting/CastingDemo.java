d5199f6b9c0764f540032eddcc4b51a77ee18220
Humphrey-nyaga <humphreynyaga254@gmail.com>
2023-09-14 01:02:40 -0400

    instanceof operator

---
+++ src/main/java/com/systechafrica/part2/casting/CastingDemo.java
@@ -0,0 +1,26 @@
+package com.systechafrica.part2.casting;
+
+import com.systechafrica.part2.inheritance.animals.Animal;
+import com.systechafrica.part2.inheritance.animals.Cat;
+import com.systechafrica.part2.inheritance.animals.Dog;
+
+public class CastingDemo {
+    public static void main(String[] args) {
+        CastingDemo app = new CastingDemo();
+        Animal animal = new Dog(); //upcasting - implicit casting
+        Animal animal2 = new Cat();
+        app.printInfo(animal);
+        app.printInfo(animal2);
+
+
+    }
+    public void printInfo(Animal animal){
+        if(animal instanceof Dog dog){
+            dog.makeSound();
+        } else if (animal instanceof Cat cat) {
+            cat.makeSound();
+
+        }
+
+    }
+}
--- src/main/java/com/systechafrica/part2/inheritance/animals/Dog.java
+++ src/main/java/com/systechafrica/part2/inheritance/animals/Dog.java
@@ -5,4 +5,9 @@ public class Dog extends Animal{
     public void makeSound(){
         System.out.println("Dog barks");
     }
+
+    @Override
+    public String toString() {
+        return "Dog{}";
+    }
 }
