28e5b85173a693deb1f194d1ae3009a1ef7d2a43
Humphrey-nyaga <humphreynyaga254@gmail.com>
2023-09-24 09:13:55 -0400

    update customer info

--- src/main/java/com/systechafrica/part3/jdbc/JDBCUtils.java
+++ src/main/java/com/systechafrica/part3/jdbc/JDBCUtils.java
@@ -29,15 +29,21 @@ public class JDBCUtils {
 
         }
     }*/
-    public static Connection connect() throws SQLException, ClassNotFoundException {
+    public static Connection connect() {
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException | SQLException ex) {
+            LOGGER.info("Error encountered: " + ex.getMessage());
+        }
+        return null;
     }
-    public static void createTable()  {
+
+    public static void createTable() {
         try {
             Connection conn = connect();
             String createTableStmt = """
-                    CREATE TABLE customer(
+                    CREATE TABLE  IF  NOT EXISTS customer(
                         id  INT PRIMARY KEY AUTO_INCREMENT,
                         firstname VARCHAR(128),
                         lastname  VARCHAR(128),
@@ -45,21 +51,23 @@ public class JDBCUtils {
                         contact VARCHAR(128),
                         email VARCHAR(128));
                     \s""";
+            assert conn != null;
             PreparedStatement preparedStatement = conn.prepareStatement(createTableStmt);
             preparedStatement.executeUpdate();
             LOGGER.info("Table  customer created successfully...");
             conn.close();
-        }catch (ClassNotFoundException | SQLException e) {
-            LOGGER.info("An error was encountered: " + e.getMessage() + "\n");
+        } catch (SQLException e) {
+            LOGGER.info("A database error was encountered: " + e.getMessage() + "\n");
 
         }
     }
 
-    public static boolean insertDataToDatabase(Customer customer){
+    public static boolean insertDataToDatabase(Customer customer) {
         String insertQuery = "INSERT INTO customer(firstname, lastname, address, contact, email) VALUES (?,?,?,?,?);";
 
         try {
             Connection conn = connect();
+            assert conn != null;
             PreparedStatement preparedStatement = conn.prepareStatement(insertQuery);
             preparedStatement.setString(1, customer.getFirstName());
             preparedStatement.setString(2, customer.getLastName());
@@ -70,29 +78,63 @@ public class JDBCUtils {
             LOGGER.info("Customer added into the database correctly...");
             conn.close();
             return true;
-        }catch (SQLException | ClassNotFoundException e) {
+        } catch (SQLException e) {
             LOGGER.info("Database Error " + e.getMessage() + "\n");
+            LOGGER.info(" Customer Insert Into Database Failed... ");
         }
-        LOGGER.info(" Customer Insert Into Database Failed... ");
         return false;
     }
 
-    //TODO -> Complete the deleteCustomerBy Email method and add an update by ID
-    public void deleteCustomerByEmail(String email){
-        String deleteQuery = "DELETE FROM customer WHERE email = '';";
+    public static void deleteCustomerByEmail(String email) {
+        String deleteQuery = "DELETE FROM customer WHERE email = ?;";
         try {
             Connection conn = connect();
+            assert conn != null;
+            PreparedStatement preparedStatement = conn.prepareStatement(deleteQuery);
+            preparedStatement.setString(1, email);
+            int rowsDeleted = preparedStatement.executeUpdate();
+            if (rowsDeleted > 0) {
+                LOGGER.info("Customer with email " + email + " deleted successfully.");
+            } else {
+                LOGGER.info("No customer found with email " + email + ". No deletion performed.");
+            }
 
-        }catch (SQLException | ClassNotFoundException e){
+        } catch (SQLException e) {
             LOGGER.info("Error Deleting Customer with email" + email);
+            LOGGER.info("Error: " + e.getMessage());
 
         }
     }
 
+    public static void updateCustomerById(Customer updatedCustomer, int id) {
+        String updateQuery = "UPDATE customer SET firstname = ?, lastname = ?, address = ?, contact = ?, email = ? WHERE id = ?;";
+        try {
+            PreparedStatement preparedStatement;
+            try (Connection conn = connect()) {
+                assert conn != null;
+                preparedStatement = conn.prepareStatement(updateQuery);
+            }
+            preparedStatement.setString(1, updatedCustomer.getFirstName());
+            preparedStatement.setString(2, updatedCustomer.getLastName());
+            preparedStatement.setString(3, updatedCustomer.getAddress());
+            preparedStatement.setString(4, updatedCustomer.getContact());
+            preparedStatement.setString(5, updatedCustomer.getEmail());
+            preparedStatement.setInt(6, id);
+            preparedStatement.executeUpdate();
+            LOGGER.info("Customer of id: " + id + " has been updated successfully");
+        } catch (SQLException e) {
+            LOGGER.info("Error updating Customer with id" + id);
+            LOGGER.info("Database Error: " + e.getMessage());
+
+        }
+    }
+
+
     public static void main(String[] args) {
-        //createTable();
+        createTable();
         Customer customer = new Customer("John", "Doe", "123 Elm St", "555-123-4567", "john@example.com");
         insertDataToDatabase(customer);
+        deleteCustomerByEmail("john@example.com");
 
     }
 }
