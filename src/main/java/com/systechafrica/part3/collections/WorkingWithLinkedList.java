45bfbec0e4beb67305f819177b0126e9724d7829
Humphrey-nyaga <humphreynyaga254@gmail.com>
2023-10-01 22:18:22 -0500

    add working with linkedlist

--- src/main/java/com/systechafrica/part3/collections/WorkingWithLinkedList.java
+++ src/main/java/com/systechafrica/part3/collections/WorkingWithLinkedList.java
@@ -4,7 +4,20 @@ import java.util.LinkedList;
 
 public class WorkingWithLinkedList {
     public static void main(String[] args) {
-        LinkedList<Student> students = new LinkedList<>();
+        LinkedList<String> animals = new LinkedList<>();
+        animals.add("Dog");
+        animals.add("Cat");
+        animals.add("Cow");
+        animals.add("Horse");
+        animals.add("Goat");
+
+        System.out.println("animals.getFirst() = " + animals.getFirst());
+        System.out.println("animals.getLast() = " + animals.getLast());
+        System.out.println("animals.get(1) = " + animals.get(1));
+        System.out.println("animals.remove(2) = " + animals.remove(2));
+        System.out.println("animals = " + animals);
+        animals.set(2,"Ostrich");
+        System.out.println("animals = " + animals);
 
     }
 }
---
+++ src/main/java/com/systechafrica/part3/collections/WorkingWithQueues.java
@@ -0,0 +1,12 @@
+package com.systechafrica.part3.collections;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class WorkingWithQueues {
+    public static void main(String[] args) {
+        Queue<PrintJob> queue = new LinkedList<>();
+
+    }
+}
